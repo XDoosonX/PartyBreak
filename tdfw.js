15d140bd9f9989b1c048d136656fa12814d4725a
XDoosonX <68435214+XDoosonX@users.noreply.github.com>
2020-12-04 22:46:46 -0600

    Update tdfw.js

--- tdfw.js
+++ tdfw.js
@@ -11,7 +11,7 @@
   if (!window.requestAnimationFrame)
     window.requestAnimationFrame = function (callback, element) {
       var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 5 - (currTime - lastTime));
+      var timeToCall = Math.max(0, 10 - (currTime - lastTime));
       var id = window.setTimeout(function () {
         callback(currTime + timeToCall);
       }, timeToCall);
